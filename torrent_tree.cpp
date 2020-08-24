f80c2411d5f133356b5581b93ed3f1660b06cef8
Logan Buth <oantby@gmail.com>
2020-08-23 20:21:39 -0500

    Add announce URI argument and preliminary path recursion

--- torrent_tree.cpp
+++ torrent_tree.cpp
@@ -1,17 +1,21 @@
 #include <iostream>
 #include <filesystem>
+#include <fstream>
+#include "bencode.hpp"
+#include "sha1.hpp"
 
 using namespace std;
 
 void usage() {
-	cout << "Usage: torrent_tree <source directory> <save directory>\n"
+	cout << "Usage: torrent_tree <source directory> <save directory> <announce URI>\n"
 		"\tCreates a series of torrent files to enable full replication of the "
 		"hierarchy at \033[1msource directory\033[0m, with all files saved to "
-		"\033[1msave directory\033[0m" << endl;
+		"\033[1msave directory\033[0m.  \033[1mannounce URI\033[0m is listed "
+		"on the torrents as a valid tracker." << endl;
 }
 
 int main(int argc, char *argv[]) {
-	if (argc < 3) {
+	if (argc < 4) {
 		usage();
 		return 1;
 	}
@@ -40,6 +44,29 @@ int main(int argc, char *argv[]) {
 	}
 	
 	// because we did no error checking above, getting here should mean all is well
-	// (or exceptions would've occurred)
+	// (or exceptions would've occurred).  That's right, I just bragged about not checking for errors.
+	string announce_url = argv[3];
+	string start_path = argv[1];
+	vector<filesystem::path> path_stack {start_path};
+	for (size_t i = 0; i < path_stack.size(); i++) {
+		for (auto &entry : filesystem::directory_iterator(path_stack[i])) {
+			if (entry.is_regular_file()) {
+				bencode::BencodeVal torrent(bencode::bencode_type::dict);
+				bencode::BencodeVal info(bencode::bencode_type::dict);
+				torrent["announce"] = announce_url;
+				info["name"] = string(".");
+				info["piece length"] = 1048576;
+				bencode::BencodeVal file(bencode::bencode_type::dict);
+				file["path"] = entry.path().string().erase(0, start_path.size() + 1);
+				file["length"] = filesystem::file_size(entry.path());
+				info["files"] = vector<bencode::BencodeVal> {file};
+				torrent["info"] = info;
+				cout << torrent.toString() << endl;
+			} else if (entry.is_directory()) {
+				path_stack.push_back(entry.path());
+			}
+		}
+	}
 	
+	return 0;
 }
