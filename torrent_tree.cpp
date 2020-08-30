b92ef77223352ab3be178c8d85bdd2ef46c3d622
Logan Buth <oantby@gmail.com>
2020-08-30 18:25:36 -0500

    Move file processing to its own function

--- torrent_tree.cpp
+++ torrent_tree.cpp
@@ -48,13 +48,99 @@ bencode::BencodeVal path_to_list(const string &path, const char sep = '/') {
 #define OVERWRITE_NONE 0
 #define OVERWRITE_NEWER 1
 #define OVERWRITE_ALL 2
+	
+bool verbose = false;
+short overwrite = OVERWRITE_NONE;
+
+set<filesystem::path> ignored_dirs;
+string start_path;
+filesystem::path out_path;
+string announce_url;
+string torrent_file_name;
+
+void process_file(filesystem::directory_entry entry) {
+	if (verbose) {
+		cout << "Processing " << entry.path() << endl;
+	}
+	
+	filesystem::path file_path = out_path / entry.path().relative_path().replace_extension(".torrent");
+	filesystem::file_status out_status = filesystem::status(file_path);
+	// this will later be based on a command-line argument.
+	if (filesystem::exists(out_status)) {
+		bool skip = true;
+		if (overwrite == OVERWRITE_ALL) {
+			if (verbose) {
+				cout << "Would skip " << file_path << ", but -f specified" << endl;
+			}
+			skip = false;
+		} else if (overwrite == OVERWRITE_NEWER) {
+			if (filesystem::last_write_time(file_path) < filesystem::last_write_time(entry.path())) {
+				if (verbose) {
+					cout << "Would skip " << file_path << ", but -u specified" << endl;
+				}
+				skip = false;
+			}
+		}
+		
+		if (skip) {
+			if (verbose) {
+				cout << "Skipping " << file_path << " - already exists" << endl;
+			}
+			return;
+		} else {
+			// time to get destructive.  In case this was a directory before (okay, satan),
+			// we're going to delete recursively.
+			filesystem::remove_all(file_path);
+		}
+	}
+	
+	bencode::BencodeVal torrent(bencode::bencode_type::dict);
+	bencode::BencodeVal info(bencode::bencode_type::dict);
+	torrent["announce"] = announce_url;
+	info["name"] = torrent_file_name;
+	uint64_t file_size = filesystem::file_size(entry.path());
+	// 5120=102400/20, looks to keep .torrent files <100K with minimum
+	// possible piece length, holding a power of 2.
+	uint64_t piece_length = max(
+		min(
+			(uint64_t)1048576,
+			(uint64_t)exp2((int)log2(file_size / 5120) + 1)),
+		(uint64_t)4096);
+	info["piece length"] = piece_length;
+	bencode::BencodeVal file(bencode::bencode_type::dict);
+	file["path"] = path_to_list(entry.path().string().erase(0, start_path.size() + 1));
+	file["length"] = file_size;
+	info["files"] = vector<bencode::BencodeVal> {file};
+	info["pieces"] = string();
+	info["private"] = 1;
+	
+	ifstream ifile(entry.path(), ios::in|ios::binary);
+	if (!ifile) {
+		perror("failed to open file");
+	}
+	string buff(piece_length, '\0');
+	while (ifile.read(&buff[0], piece_length)) {
+		info["pieces"] += sha1::hash(buff);
+	}
+	if (ifile.eof()) {
+		// loop finished with data left.  add that data.
+		buff.resize(ifile.gcount());
+		if (buff.size()) {
+			info["pieces"] += sha1::hash(buff);
+		}
+	}
+	torrent["info"] = info;
+	
+	if (verbose) {
+		cout << "Creating " << file_path << endl;
+	}
+	filesystem::create_directories(file_path.parent_path());
+	ofstream ofile(file_path, ios::out|ios::trunc);
+	ofile << torrent.toString();
+	ofile.close();
+}
 
 int main(int argc, char *argv[]) {
-	
-	bool verbose = false;
-	short overwrite = OVERWRITE_NONE;
-	
-	set<filesystem::path> ignored_dirs;
 	struct option long_options[] = {
 		{"ignore", required_argument, 0, 0},
 		{0, 0, 0, 0}
@@ -100,9 +186,9 @@ int main(int argc, char *argv[]) {
 		usage();
 		return 1;
 	}
-	string start_path = argv[optind];
-	filesystem::path out_path(argv[optind + 1]);
-	string announce_url = argv[optind + 2];
+	start_path = argv[optind];
+	out_path = argv[optind + 1];
+	announce_url = argv[optind + 2];
 	
 	if (!filesystem::is_directory(filesystem::status(start_path))) {
 		cerr << "No such directory: " << start_path << endl;
@@ -131,91 +217,16 @@ int main(int argc, char *argv[]) {
 	// (or exceptions would've occurred).  That's right, I just bragged about not checking for errors.
 	if (start_path.back() == '/') start_path.pop_back();
 	vector<filesystem::path> path_stack {start_path};
-	string torrent_file_name = path_stack.back().has_filename() ? path_stack.back().filename()
-		: (path_stack.back().has_parent_path() ? path_stack.back().parent_path().filename() : "files");
+	torrent_file_name = path_stack.back().has_filename() ? path_stack.back().filename()
+		: (path_stack.back().has_parent_path() ? path_stack.back().parent_path().filename() : ".");
+	// there's a couple different ways for just dot as a name.
+	if (torrent_file_name == ".") torrent_file_name = "files";
 	
 	for (size_t i = 0; i < path_stack.size(); i++) {
 		for (auto &entry : filesystem::directory_iterator(path_stack[i])) {
 			if (entry.is_regular_file()) {
-				if (verbose) {
-					cout << "Processing " << entry.path() << endl;
-				}
 				
-				filesystem::path file_path = out_path / entry.path().relative_path().replace_extension(".torrent");
-				filesystem::file_status out_status = filesystem::status(file_path);
-				// this will later be based on a command-line argument.
-				if (filesystem::exists(out_status)) {
-					bool skip = true;
-					if (overwrite == OVERWRITE_ALL) {
-						if (verbose) {
-							cout << "Would skip " << file_path << ", but -f specified" << endl;
-						}
-						skip = false;
-					} else if (overwrite == OVERWRITE_NEWER) {
-						if (filesystem::last_write_time(file_path) < filesystem::last_write_time(entry.path())) {
-							if (verbose) {
-								cout << "Would skip " << file_path << ", but -u specified" << endl;
-							}
-							skip = false;
-						}
-					}
-					
-					if (skip) {
-						if (verbose) {
-							cout << "Skipping " << file_path << " - already exists" << endl;
-						}
-						continue;
-					} else {
-						// time to get destructive.  In case this was a directory before (okay, satan),
-						// we're going to delete recursively.
-						filesystem::remove_all(file_path);
-					}
-				}
-				
-				bencode::BencodeVal torrent(bencode::bencode_type::dict);
-				bencode::BencodeVal info(bencode::bencode_type::dict);
-				torrent["announce"] = announce_url;
-				info["name"] = torrent_file_name;
-				uint64_t file_size = filesystem::file_size(entry.path());
-				// 5120=102400/20, looks to keep .torrent files <100K with minimum
-				// possible piece length, holding a power of 2.
-				uint64_t piece_length = max(
-					min(
-						(uint64_t)1048576,
-						(uint64_t)exp2((int)log2(file_size / 5120) + 1)),
-					(uint64_t)4096);
-				info["piece length"] = piece_length;
-				bencode::BencodeVal file(bencode::bencode_type::dict);
-				file["path"] = path_to_list(entry.path().string().erase(0, start_path.size() + 1));
-				file["length"] = file_size;
-				info["files"] = vector<bencode::BencodeVal> {file};
-				info["pieces"] = string();
-				info["private"] = 1;
-				
-				ifstream ifile(entry.path(), ios::in|ios::binary);
-				if (!ifile) {
-					perror("failed to open file");
-				}
-				string buff(piece_length, '\0');
-				while (ifile.read(&buff[0], piece_length)) {
-					info["pieces"] += sha1::hash(buff);
-				}
-				if (ifile.eof()) {
-					// loop finished with data left.  add that data.
-					buff.resize(ifile.gcount());
-					if (buff.size()) {
-						info["pieces"] += sha1::hash(buff);
-					}
-				}
-				torrent["info"] = info;
-				
-				if (verbose) {
-					cout << "Creating " << file_path << endl;
-				}
-				filesystem::create_directories(file_path.parent_path());
-				ofstream ofile(file_path, ios::out|ios::trunc);
-				ofile << torrent.toString();
-				ofile.close();
+				process_file(entry);
 				
 			} else if (entry.is_directory()) {
 				filesystem::path t = filesystem::absolute(entry.path());
